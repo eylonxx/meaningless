bd02a3e6c857b7426b0d3a5fe78c8886990ab0fc
Eylon <eylon.perets@gmail.com>
2024-06-08 16:29:05 +0300

    add supabase db

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,12 @@
+import { db } from "@/drizzle/db";
+import { users } from "@/drizzle/schema";
 import { getServerSession } from "next-auth";
 import React from "react";
 
 const page = async () => {
   const session = await getServerSession();
+  const allUsers = await db.select().from(users);
+
   const user = session?.user;
 
   if (!user) {
